6f8f744dd6f9ce5af7e3495552f489c8bbf0eeec
Youssra <youssra.outelli@gmail.com>
2018-11-29 11:10:41 +0100

    added tableView that displays an array with 3 values

--- RxSwiftTest/RxSwiftTest/ViewController.swift
+++ RxSwiftTest/RxSwiftTest/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+  private var tableView: UITableView!
+  private let myArray = ["First", "Second", "Third"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+    let displayWidth: CGFloat = self.view.frame.width
+    let displayHeight: CGFloat = self.view.frame.height
+
+    tableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    tableView.dataSource = self
+    tableView.delegate = self
+    self.view.addSubview(tableView)
   }
 
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return myArray.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+    cell.textLabel!.text = "\(myArray[indexPath.row])"
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("Num: \(indexPath.row)")
+    print("Value: \(myArray[indexPath.row])")
+  }
 
 }
 
